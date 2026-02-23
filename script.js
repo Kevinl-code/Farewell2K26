707cbc83b44b7d6bcc0e7bbaefbeb51aa5f3c0ef
Kevin Lazarus B <kevinlazarus2003@gmail.com>
2026-02-23 13:01:59 +0530

    Update reveal time and clean up code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 
-const revealTime = new Date("Feb 23, 2026 1:00:00").getTime();
+const revealTime = new Date("Feb 23, 2026 1:03:00").getTime();
 
 const timer = document.getElementById("timer");
 
@@ -183,4 +183,5 @@ document.getElementById("modal").onclick=()=>{
  document.getElementById("modal").style.display="none";
 
 };
+
 
